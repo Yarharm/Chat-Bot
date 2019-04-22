14a9779aa1dbec4a47bfea0b1e97f4742c01739e
Yarharm <yarharm@gmail.com>
2019-04-21 21:51:15 -0400

    Finish sending processed data to the Interpeter JSON

--- data/raw_data_parser.py
+++ data/raw_data_parser.py
@@ -2,20 +2,18 @@ import json
 import pandas as pd
 import os
 
-# Get Recipe texts
-#recipe_text = pd.read_csv(os.getcwd() + '/raw_data/recipes.csv', low_memory=False)
-#recipe_text = recipe_text['title']
-# print(recipe_text.shape)
-# print(recipe_text[0])
 class RawDataParser:
     """
-    Every recipe and ingredient processed as *Lowercase*
+    Parse for recipes and ingredients from the files found at:
+        'https://www.kaggle.com/hugodarwood/epirecipes#epi_r.csv'
+    And write processed results to the Interpreter data.json
+
+    NOTE: Every recipe and ingredient processed as *Lowercase* only
     """
     def __init__(self):
         self.recipes = os.getcwd() + '/raw_data/recipes.csv'
         self.ingredients = os.getcwd() + '/raw_data/ingredients.json'
-        self.LEFT_CURLY = '{'
-        self.RIGHT_CURLY = '}'
+        self.greet_goodbye = os.getcwd() + '/raw_data/greet_goodbye.json'
 
     def get_recipes_list(self):
         """
@@ -62,7 +60,51 @@ class RawDataParser:
                 resultDict[entity] = recipe.find(entity)
         return resultDict
 
-    def write_interpreter_object(self):
+    def process_recipe(self, recipe, entities_info):
+        """
+        Process recipe example to match Rasa NLU style
+        """
+        entities_processed = []
+        single_entity = {}
+        for entity, entity_start_index in entities_info.items():
+            single_entity["start"] = entity_start_index
+            single_entity["end"] = entity_start_index + len(entity)
+            single_entity["value"] = entity
+            single_entity["entity"] = "ingredient"
+            entities_processed.append(single_entity)
+            single_entity = {}
+
+        single_recipe_processed = {"text": recipe,
+                                   "intent": "inform",
+                                   "entities": entities_processed}
+        return single_recipe_processed
+
+    def write_processed_info(self):
+        # Prepare json file for Rasa Interpreter
+        MAIN_FILE = {}
+        COMMON_EXAMPLES = {}
+        processed_objects = []
+        MAIN_FILE["rasa_nlu_data"] = COMMON_EXAMPLES
+        COMMON_EXAMPLES["common_examples"] = processed_objects
+
+        # Add greet/goodbye information
+        with open(self.greet_goodbye) as json_f:
+            greet_bye_info = json.load(json_f)
+            processed_objects.extend(greet_bye_info["first_layer"]["second_layer"])
+
+        # Get data
+        recipes = self.get_recipes_list()
+        ingredients = self.get_ingredient_list()
+        for index, recipe in enumerate(recipes):
+            entities_info = self.get_entities_from_recipe(recipe, ingredients)
+            recipe_processed = self.process_recipe(recipe, entities_info)
+
+            # Add to the processed object wrapper
+            processed_objects.append(recipe_processed)
+
+        # Write data
+        with open(os.getcwd() + '/data.json', 'w') as outfile:
+            json.dump(MAIN_FILE, outfile, indent=2)
 
     def get_no_entities_recipes(self, recipes, ingredients):
         useless = {}
@@ -73,7 +115,4 @@ class RawDataParser:
         return useless
 
 parser = RawDataParser()
-recipes = parser.get_recipes_list()
-ingr = parser.get_ingredient_list()
-
-
+parser.write_processed_info()
