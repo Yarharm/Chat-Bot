5eaabc94831bd4e3445d72871af17007b98c9065
Yarharm <yarharm@gmail.com>
2019-04-27 21:23:57 -0400

    Finish Food2Fork custom API setup

---
+++ food2fork.py
@@ -0,0 +1,79 @@
+import urllib.request
+import urllib.parse
+import json
+
+class Food2ForkClient():
+    """
+    Castom imlementation of Food2Fork API for recipes retrival.
+
+    Use case:
+                from food2fork import Food2ForkClient
+                f2fClient = Food2ForkClient('Your Api Key')
+
+                recipes = f2fClient.search('noodles', 'pork')
+
+                # Next page
+                moreRecipes = f2fClient.search('noodles', 'pork', 2)
+    """
+
+    def __init__(self, apiKey, debug=False):
+        self.apiKey = apiKey
+        self.debugMode = debug
+
+        self.SEARCH = 'http://food2fork.com/api/search'
+        self.VIEW = 'http://food2fork.com/api/get'
+
+    def search(self, query, page=1, pageSize=30):
+        try:
+            url = self.urlHelper(self.SEARCH, q=query, page=page, count=pageSize)
+
+            contents = self.getUrlContents(url)
+
+            data = json.loads(contents)
+
+            if self.debugMode:
+                print(contents)
+
+            return data
+
+        except Exception as instance:
+            if self.debugMode:
+                print(instance)
+
+            return None
+
+    def urlHelper(self, endpoint, **kwargs):
+        """
+        Build Url for POST request
+        :param endpoint:
+        :param kwargs:
+        :return:
+        """
+        data = {'key': self.apiKey}
+
+        for key, value in kwargs.items():
+            data[key] = value
+
+        if self.debugMode:
+            print('Url: ', endpoint + '?' + urllib.parse.urlencode(data))
+
+        return endpoint + '?' + urllib.parse.urlencode(data)
+
+    def getUrlContents(self, url):
+        """
+        Decypher url contents
+        :param url:
+        :return:
+        """
+        try:
+            response = urllib.request.urlopen(url)
+
+            contents = response.read()
+
+            return contents
+
+        except Exception as instance:
+            if self.debugMode:
+                print(instance)
+
+            return None
